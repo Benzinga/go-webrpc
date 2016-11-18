4620018429ab932ef18674aadd7bfa1d476b24f0
John Chadwick <johnwchadwick@gmail.com>
2016-11-18 12:03:44 -0500

    Flip the switch! Compression on.
    This enables compression for Gorilla Websockets.

--- server.go
+++ server.go
@@ -8,8 +8,9 @@ import (
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+	ReadBufferSize:  4096,
+	WriteBufferSize: 4096,
+	EnableCompression: true,
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
